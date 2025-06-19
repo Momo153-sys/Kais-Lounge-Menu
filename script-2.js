acc83e2a4395cc10a6316827e09700f50c1ba611
Momo153-sys <154274664+Momo153-sys@users.noreply.github.com>
2025-06-19 23:45:06 +0300

    Update script-2.js

--- script-2.js
+++ script-2.js
@@ -1,11 +1,11 @@
 const meals = {
-    name: ['Tost','Espresso','Burger','aparitifler','pizza','filtre kahve','türk kahvesi','salep','sıcak içecekler',
-        'soğuk kahve','milkshake','bitki çayı','frozen','soğuk içecek','smoothie','kokteyl','vitamin bar','tatlı',
-        'nargile','dondurma','ana yemek','makarna','wrap'
+    name: ['soğuk içecek','Espresso','soğuk kahve','vitamin bar','kokteyl','dondurma','milkshake','smoothie','filtre kahve','tatlı','nargile','türk kahvesi','salep','sıcak içecekler',
+    'frozen','Burger','bitki çayı','Tost','pizza','aparitifler','ana yemek','makarna','wrap'
     ],
-    image: ['tost.jpeg','espresso.webp','burger.jpg','aparitif.jpeg','pizza.jpg','filtre-kahve.jpeg','turk-kahvesi.jpeg','salep.jpeg',
-        'sicak-icecek.jpeg','soguk-kahve.jpeg','milkshake.jpg','bitki-cayi.jpeg','frozen.jpeg','soguk-icecek.jpeg','smoothie.jpeg','kokteyl.jpeg','vitamin-bar.jpeg',
-        'tatli.jpeg','nargile.jpeg','dondurma.jpeg','tavuk-izgara.jpg','tavuklu-penne.jpg','tavuklu-wrap.jpg'
+    image: ['soguk-icecek.jpeg','espresso.webp','soguk-kahve.jpeg','vitamin-bar.jpeg','kokteyl.jpeg',
+    'dondurma.jpeg','milkshake.jpg','smoothie.jpeg','filtre-kahve.jpeg','tatli.jpeg','nargile.jpeg',
+    'turk-kahvesi.jpeg','salep.jpeg','sicak-icecek.jpeg','frozen.jpeg','burger.jpg','bitki-cayi.jpeg',
+    'tost.jpeg','pizza.jpg','aparitif.jpeg','tavuk-izgara.jpg','tavuklu-penne.jpg','tavuklu-wrap.jpg'
     ],
     path: ['tost','espresso','burger','aparitif','pizza','filtre-kahve','turk-kahvesi','salep','sicak-icecek',
         'soguk-kahve', 'milkshake','bitki-cayi','frozen','soguk-icecek','smoothie','kokteyl','vitamin-bar' ,'tatli','nargile','dondurma',
@@ -27,3 +27,4 @@ for (let index = 0; index < meals.name.length; index++) {
 }
 
 document.getElementById('food-category').innerHTML = content;
+
