b501a3900f25f046469901ef771d6ba3438e6186
Momo153-sys <154274664+Momo153-sys@users.noreply.github.com>
2025-06-19 22:17:52 +0300

    Update script-2.js

--- script-2.js
+++ script-2.js
@@ -1,15 +1,15 @@
 const meals = {
-    name: ['Gözleme','Tost','Espresso','Burger','aparitifler','pizza','filtre kahve','türk kahvesi','salep','sıcak içecekler',
-        'soğuk kahve','kırmızı et','milkshake','bitki çayı','frozen','soğuk içecek','smoothie','kokteyl','vitamin bar','tatlı',
-        'nargile','dondurma','ana yemek','makarna','wrap','salata'
+    name: ['Tost','Espresso','Burger','aparitifler','pizza','filtre kahve','türk kahvesi','salep','sıcak içecekler',
+        'soğuk kahve','milkshake','bitki çayı','frozen','soğuk içecek','smoothie','kokteyl','vitamin bar','tatlı',
+        'nargile','dondurma','ana yemek','makarna','wrap'
     ],
-    image: ['gozleme.jpeg','tost.jpeg','espresso.webp','burger.jpg','aparitif.jpeg','pizza.jpg','filtre-kahve.jpeg','turk-kahvesi.jpeg','salep.jpeg',
-        'sicak-icecek.jpeg','soguk-kahve.jpeg','kirmizi-et.jpg','milkshake.jpg','bitki-cayi.jpeg','frozen.jpeg','soguk-icecek.jpeg','smoothie.jpeg','kokteyl.jpeg','vitamin-bar.jpeg',
-        'tatli.jpeg','nargile.jpeg','dondurma.jpeg','tavuk-izgara.jpg','tavuklu-penne.jpg','tavuklu-wrap.jpg','helim-salata.jpg'
+    image: ['tost.jpeg','espresso.webp','burger.jpg','aparitif.jpeg','pizza.jpg','filtre-kahve.jpeg','turk-kahvesi.jpeg','salep.jpeg',
+        'sicak-icecek.jpeg','soguk-kahve.jpeg','milkshake.jpg','bitki-cayi.jpeg','frozen.jpeg','soguk-icecek.jpeg','smoothie.jpeg','kokteyl.jpeg','vitamin-bar.jpeg',
+        'tatli.jpeg','nargile.jpeg','dondurma.jpeg','tavuk-izgara.jpg','tavuklu-penne.jpg','tavuklu-wrap.jpg'
     ],
-    path: ['gozleme','tost','espresso','burger','aparitif','pizza','filtre-kahve','turk-kahvesi','salep','sicak-icecek',
-        'soguk-kahve','kirmizi-et', 'milkshake','bitki-cayi','frozen','soguk-icecek','smoothie','kokteyl','vitamin-bar' ,'tatli','nargile','dondurma',
-        'ana-yemek','makarna','wrap','salata'
+    path: ['tost','espresso','burger','aparitif','pizza','filtre-kahve','turk-kahvesi','salep','sicak-icecek',
+        'soguk-kahve', 'milkshake','bitki-cayi','frozen','soguk-icecek','smoothie','kokteyl','vitamin-bar' ,'tatli','nargile','dondurma',
+        'ana-yemek','makarna','wrap'
     ]
 };
 let content = '';
@@ -20,7 +20,7 @@ for (let index = 0; index < meals.name.length; index++) {
         <img src="images/${meals.image[index]}" class="card-img-top" alt="...">
         <div class="card-body">
             
-            <a href="/products.html/#${meals.path[index]}" class="btn btn-lg btn-warning">${meals.name[index]}</a>
+            <a href="products.html#${index}" class="btn btn-lg btn-warning">${meals.name[index]}</a>
         </div>
         </div>
     `
